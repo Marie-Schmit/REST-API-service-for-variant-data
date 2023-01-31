2fd6c7965fc958689ddb663ca67aa4f0eaf47110
Marie-Schmit <marie.schmit3003@gmail.com>
2023-01-31 23:33:45 +0000

    start of R coding for density plotting

--- REST_API/router.js
+++ REST_API/router.js
@@ -200,38 +200,11 @@ variant_router.get('/variants/density/:genome/:chromosome/:windowSize/:type?/:su
             //Get array of variant density accross every chosen windows
             //var varDensity = variantDensity(Number(rows[0].maxPos), parameters, Number(req.params.windowSize));
             //Save result in a json and display
-            Promise.all([ variantDensity(Number(rows[0].maxPos), parameters, Number(req.params.windowSize))]).then(function([varDensity]){
-                //Json creation 
-                var resultDensity = {
-                };
-
-                resultDensity.Genome = parameters[0];
-                resultDensity.Chromosome = parameters[1];
-                if(parameters.length > 2){
-                    resultDensity.Type = parameters[2];
-                    if(parameters.length > 3){
-                        resultDensity.Subtype = parameters[3];
-                    }
-                }
-
-                //Show window size
-                resultDensity.WindowSize = req.params.windowSize;
-                //Array containing a list of windows and their density
-                resultDensity.Window = [];
-
-                for(var i = 0; i < varDensity.length; i++){
-                    //Number of window
-                    var windowDens = {
-                    };
-
-                    windowDens.StartPosition = i*req.params.windowSize;
-                    windowDens.EndPosition = (1+i)*req.params.windowSize;
-                    windowDens.Density = varDensity[i];
-
-                    resultDensity.Window.push(windowDens);
-                }
-
-
+            Promise.all([variantDensity(Number(rows[0].maxPos), parameters, Number(req.params.windowSize))]).then(function ([varDensity]) {
+                console.log("in");
+                var resultDensity = jsonResult(parameters, req.params.windowSize, varDensity);
+                console.log(resultDensity);
+                console.log("out");
                 res.json(resultDensity);
             })
         });
@@ -241,7 +214,6 @@ variant_router.get('/variants/density/:genome/:chromosome/:windowSize/:type?/:su
 async function variantDensity(maxPosition, parameters, windowSize) {
     //Store maximal number of windows
     maxWindows = Math.ceil(maxPosition / windowSize); //Results sent as JSON objects
-    
 
     console.log("Win size: " + windowSize);
     console.log("Max position: " + maxPosition);
@@ -259,7 +231,7 @@ async function variantDensity(maxPosition, parameters, windowSize) {
         //For the current window, calculate value of density and add it to array
         //variantDensity.push(calculateDensity(maxPosition, parameters, startPosition, endPosition));
 
-        promise = await(calculateDensity(maxPosition, parameters, startPosition, endPosition));
+        promise = await (calculateDensity(maxPosition, parameters, startPosition, endPosition));
         variantDensity.push(promise);//Get value of promise function (async)
 
         startPosition += windowSize;
@@ -268,7 +240,7 @@ async function variantDensity(maxPosition, parameters, windowSize) {
 
     console.log("var density final: " + variantDensity);
     //Return array of variant density
-    return(variantDensity);
+    return (variantDensity);
 }
 
 async function calculateDensity(maxPosition, parameters, startPosition, endPosition) {
@@ -276,14 +248,14 @@ async function calculateDensity(maxPosition, parameters, startPosition, endPosit
         'JOIN variants_observed ON variants_observed.variant_id = variants.variant_id ' +
         'JOIN genomes ON variants_observed.genome_id = genomes.genome_id ' +
         'WHERE genomes.genome_id = ? AND variants.chromosome = ?';
-   
+
     const endQuery = ' AND variants.position > ? AND variants.position <= ?;';
 
     //Add start and end position of the window to parameters
     var newParam = [];
     var j;
     //Parameters need to be copied in a new instance. Otherwise, the array will be modified at each loop.
-    for (j = 0; j < parameters.length; j++){
+    for (j = 0; j < parameters.length; j++) {
         newParam.push(parameters[j]);
     }
 
@@ -305,18 +277,57 @@ async function calculateDensity(maxPosition, parameters, startPosition, endPosit
     var variantDensity = await getCounts(db, query, newParam);
 
     console.log("density: " + variantDensity.density);
-    return(variantDensity.density);
-}   
+    return (variantDensity.density);
+}
 
 
 //Callback used in sqlite3.Database.all is async: the code is executed without any waiting.
 //Variable cannot be assigned into the callback function of all without waiting.
 //Creation of asyncheonous function to get result from database.
-async function getCounts(db, query, newParam){
-    return new Promise(function(resolve, reject){
-        db.get(query, newParam, function(err, row){
+async function getCounts(db, query, newParam) {
+    return new Promise(function (resolve, reject) {
+        db.get(query, newParam, function (err, row) {
             if (err) reject(err);
             resolve(row);
         });
     });
+}
+
+//Function that create a json to store the array of variants located in a specific region of a specific chromosome in a specific dataset
+function jsonResult(parameters, windowSize, varDensity) {
+    //Json creation 
+    var resultDensity = {
+    };
+    //Result to show is the list of variants for the given window and parameters
+    //Add genome and chromosome
+    resultDensity.Genome = parameters[0];
+    resultDensity.Chromosome = parameters[1];
+
+    //If they were given, specify type and subtype
+    if (parameters.length > 2) {
+        resultDensity.Type = parameters[2];
+        if (parameters.length > 3) {
+            resultDensity.Subtype = parameters[3];
+        }
+    }
+
+    //Add window size
+    resultDensity.WindowSize = windowSize;
+    //Array containing a list of windows and their density
+    resultDensity.Window = [];
+
+    for (var i = 0; i < varDensity.length; i++) {
+        //Number of window
+        var windowDens = {
+        };
+        //Specify the window (start and end position) and it's coresponding density in an object
+        windowDens.StartPosition = i * windowSize;
+        windowDens.EndPosition = (1 + i) * windowSize;
+        windowDens.Density = varDensity[i];
+
+        //Save the object containing window specification in the results array
+        resultDensity.Window.push(windowDens);
+    }
+
+    return resultDensity;
 }
---
+++ REST_API/variant_density.R
@@ -0,0 +1,12 @@
+library(plumber)
+
+#* @apiTitle 
+#* @param name
+#* @get /variants/density/plot
+function(genome, chromosome, windowSize, type, subtype){
+  if(type != null){
+    if(subtype != null){
+      
+    }
+  }
+}
