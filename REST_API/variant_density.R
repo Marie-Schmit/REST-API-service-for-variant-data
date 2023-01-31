ab2e19fa0a00850977bcc6ab1fab1e120665aba1
Marie-Schmit <marie.schmit3003@gmail.com>
2023-01-31 23:54:05 +0000

    Plumber request with /<parameter> in url

--- REST_API/variant_density.R
+++ REST_API/variant_density.R
@@ -1,12 +1,33 @@
 library(plumber)
 
-#* @apiTitle 
-#* @param name
-#* @get /variants/density/plot
-function(genome, chromosome, windowSize, type, subtype){
-  if(type != null){
-    if(subtype != null){
-      
+#* @apiTitle
+
+#* @serializer png
+#* @get /api/variants/density/plot/<genome>/<chromosome>/<windowSize>/<type>/<subtype>
+function(genome, chromosome, windowSize, type = "", subtype = ""){
+  if(type != ""){ 
+    #Type is given as argument
+    if(subtype != ""){
+      #Subtype is given as parameter
+      request <- GET(paste("http://localhost:3000/api/variants/density/"), 
+                     genome, "/", chromosome, "/", windowSize, "/", type, sep = '')
+    }
+    else{ 
+      #Subtype not provided, only type is a parameter
+      request <- GET(paste("http://localhost:3000/api/variants/density/"), 
+                     genome, "/", chromosome, "/", windowSize, sep = '')
     }
   }
+  else{
+    #Neither type nor subtype are parameters
+    request <- GET(paste("http://localhost:3000/api/variants/density/"), 
+                   genome, "/", chromosome, "/", windowSize, "/", type, "/", subtype, sep = '')
+  }
+  
+  response <- content(request, as = "text", encoding = "UTF-8")
+  df <- fromJSON(response)
+  print(df);
 }
+
+
+#:genome/:chromosome/:windowSize/:type?/:subtype?
