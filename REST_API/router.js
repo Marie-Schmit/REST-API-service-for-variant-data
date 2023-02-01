9016a7a965666d1c89feae5d03620beb7d280e0e
Marie-Schmit <marie.schmit3003@gmail.com>
2023-02-01 15:45:37 +0000

    function for zygotie

--- REST_API/router.js
+++ REST_API/router.js
@@ -137,6 +137,92 @@ variant_router.get('/variants/region/:genome/:chromosome/:startPosition/:endPosi
     });
 });
 
+
+//Return a list of heterozygotes or homozygotes variants located in a specific chromosome in a specific dataset
+variant_router.get('/variants/zygosity/:genome/:chromosome/:zygosity/:startPosition/:endPosition/:type?/:subtype?', function (req, res) {
+    var query = 'SELECT chromosome AS chromosome, position AS position, (variants.position + LENGTH(variants.alteration)) AS end, reference AS ref, alteration AS alt, ' +
+        'genomes.genome_id AS genome, ' +
+        'variants_observed.quality AS qual, variants_observed.filter AS filter, ' +
+        'infos.extra_info AS info, infos.info_format, infos.info_values ' +
+        'FROM variants JOIN variants_observed ON variants.variant_id = variants_observed.variant_id ' +
+        'JOIN genomes ON variants_observed.genome_id = genomes.genome_id ' +
+        'JOIN infos ON variants_observed.info_id = infos.info_id ' +
+        'WHERE genomes.genome_id = ? AND chromosome = ? ' +
+        'AND variants.position >= ? AND variants.position <= ? AND (variants.position + LENGTH(variants.alteration)) <= ? '
+
+
+    console.log(req.params.zygosity);
+
+    //Query parameters taking place of placeholdes in SQL query string
+    var parameters = [
+        req.params.genome,
+        req.params.chromosome,
+        req.params.startPosition,
+        req.params.endPosition,
+        req.params.endPosition
+    ];
+
+    //If no type of variant precised
+    if (req.params.type) {
+        if (req.params.subtype) {
+            query += 'AND variants.var_type = ? AND variants.var_subtype = ? ';
+            //Add types and subtypes to parameters
+            parameters.push(req.params.type);
+            parameters.push(req.params.subtype);
+        }
+        else {
+            query +='AND variants.var_type = ? ';
+            //Add types and subtypes two parameters
+            parameters.push(req.params.type);
+        }
+    }
+
+    //Search for homozygotie or heterozygotie
+    if(req.params.zygosity == "Homozygote"){
+        query += 'AND infos.info_format LIKE "%GT%" AND infos.info_values LIKE "%1|1%" ';
+    }
+    else if(req.params.zygosity == "Heterozygote"){
+        query += 'AND infos.info_format LIKE "%GT%" AND (infos.info_values LIKE "%1|0%" OR infos.info_values LIKE "%0|1%")';
+    }
+
+    console.log(query);
+    
+    //Add last line to query
+    query += 'ORDER BY variants.chromosome;';
+
+    console.log(parameters);
+    db.all(query, parameters, function (err, rows) {
+        if (err) {
+            throw err;
+        }
+        console.log(rows);
+        for(i = 0; i < rows.length; i++){
+            //Get info format and values
+            var format = rows[i].info_format;
+            var values = rows[i].info_values;
+
+            //Split on ":"
+            var formats = format.split(":");
+            var values = values.split(":");
+
+            //Store detailed information in a json
+            var information = {};
+            for(var j = 0; j < formats.length; j++){
+                information[formats[j]] = values[j];
+            }
+
+            //Replace detailed information values in rows
+            delete(rows[i].info_format);
+            //Replace info_values with the JSON
+            rows[i].info_values = information;
+        }
+        console.log(rows);
+        res.json(rows); //Results sent as JSON objects
+    });
+});
+
+
+
 //Using an R script, report and plot the variants (SNPNs, InDels or both) density for a specific window size
 //accross a specific chromosome and for a specific genome
 variant_router.get('/variants/density/:genome/:chromosome/:windowSize/:type?/:subtype?', function (req, res) {
